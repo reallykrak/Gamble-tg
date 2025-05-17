fef438465f91c8f535f9df0358b7c44d8b13de66
krak <notreallykrak@gmail.com>
2025-05-17 16:49:13 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+# config.py
+# 💡 Botun temel ayarlarını burada tanımlıyoruz!
+TOKEN = "YOUR_API_TOKEN"  # @BotFather’dan aldığın API token!
+CHANNEL_LINK = "https://t.me/addlist/h49pICAbWT81OGI0"  # Kullanıcıların katılmasını istediğin kanal
+ADMIN_IDS = [12345678, 98765432]  # 👑 Bot yönetimi için admin Telegram ID’leri
